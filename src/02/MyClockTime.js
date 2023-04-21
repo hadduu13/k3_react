fcae74785242a6a5f8929adff7abbe18f8408582
ehadduu13 <haeju813@gmail.com>
2023-04-21 11:05:05 +0900

    수정

---
+++ src/02/MyClockTime.js
@@ -0,0 +1,13 @@
+const MyClockTime = () => {
+
+    // let myTime = new Date().toLocalTimeString();
+    let myTime = new Date().toLocaleTimeString();
+
+    return (
+        <footer>
+            <h1>{myTime}</h1>
+        </footer>
+    );
+}
+
+export default MyClockTime;
