b4f4461417d8380c8b42a23efeac021ee8b4bd2a
hadduu13 <haeju813@gmail.com>
2023-05-13 20:54:52 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 //import Frcst from './05/Frcst';
 //import Taccident from './06/Taccident';
 //import MyRef from './07/MyRef';
-import Gellery from './08/Gallery';
+import Gallery from './08/Gallery';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
     //<Frcst />
     //<Taccident />
     //<MyRef />
-    <Gellery />
+    <Gallery />
   );
 }
 
